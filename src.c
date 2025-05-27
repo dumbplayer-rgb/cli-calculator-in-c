f5baab94bf0d45c1764dfbcc98f111b16ee51096
dumbplayer-rgb <maxtomasik6901@gmail.com>
2025-05-27 16:28:29 +0100

    Add files via upload

---
+++ src.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	printf("1: addition\n");
+	printf("2: subtraction\n");
+	printf("3: multiplication\n");
+	printf("4: division\n");
+	int selection = scanf("selection");
+	if selection == 1:
+		printf("hello");
+	return 0;
+}
