1532c33a53f4ae70ef803eea5273aa4c83774276
dumbplayer-rgb <maxtomasik6901@gmail.com>
2025-05-31 03:27:57 +0100

    changed from an if command to a switch case

--- src.c
+++ src.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+
 int main(){
 	printf("1: addition\n");
 	printf("2: subtraction\n");
 	printf("3: multiplication\n");
 	printf("4: division\n");
-	int selection = scanf("selection");
-	if selection == 1:
-		printf("addition selected");
+	
+	int selection;
+	scanf("%d",&selection);
+
+	switch(selection){
+		case 1:
+			printf("Addition selected\n");
+			break;
+		case 2: 
+			printf("subtraction selected\n");
+			break;
+		case 3:
+			printf("multiplication selected\n");
+			break;
+		case 4:
+			printf("division selected\n");
+			break;
+		default:
+			printf("Invalid selection\n");
+			break;
+	}
 	return 0;
 }
